50fdf4f323ad786baf33277621deccdc54f0ff87
selene_gabriela <sel_nn@hotmail.com>
2026-02-19 21:18:44 -0600

    creating the edit project form

---
+++ src/components/projects/EditProjectForm.tsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom"
+import ProjectForm from "./ProjectForm"
+import type { ProjectFormData } from "@/types/index";
+import { useForm } from "react-hook-form";
+
+
+const EditProjectForm = () => {
+    const initialValues: ProjectFormData = {
+        projectName: "",
+        clientName: "",
+        description: "",
+    }
+
+    const {register, handleSubmit, formState: {errors}} = useForm({defaultValues: initialValues});
+
+
+
+    const handleForm = () => {
+
+    }
+
+  return (
+   <>
+      <div className="max-w-3xl mx-auto">
+
+        <h1 className='text-5xl font-black'>Edit Project</h1>
+        <p className='text-2xl font-light text-gray-500 mt-5'>Fill the form to edit the project</p>
+
+        <nav className="my-5">
+            <Link  
+                className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors" to={'/'}>
+                Back to Projects
+            </Link>
+        </nav>
+
+        <form
+            className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+            onSubmit={handleSubmit(handleForm)}
+            noValidate
+            >
+            
+            <ProjectForm 
+                register={register}
+                errors={errors}
+            />
+            <input 
+                type="submit"
+                value="Keep changes"
+                className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+            />
+
+
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default EditProjectForm
--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -1,22 +1,22 @@
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from '@tanstack/react-query';
 import { getProjectById } from "@/api/ProjectApi";
+import EditProjectForm from "@/components/projects/EditProjectForm";
 
 
 const EditProjectView = () => {
     const params = useParams();
     const projectId = params.projectId!;
 
-    const { data, isLoading } = useQuery({
+    const { data, isLoading, isError } = useQuery({
         queryKey: ['editProject', projectId],
         queryFn: () => getProjectById(projectId),
         retry: false
     })
 
-    console.log(data, isLoading);
-  return (
-    <div>EditProjectView</div>
-  )
+    if(isLoading) return 'Loading...';
+    if(isError) return <Navigate to='/404' />
+    if(data) return <EditProjectForm />
 }
 
 export default EditProjectView
