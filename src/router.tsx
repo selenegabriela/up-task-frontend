c782bfb1adcbf69082492d4bbaa9bafcf34f7782
selene_gabriela <sel_nn@hotmail.com>
2026-03-01 22:27:39 -0600

    Routing details project

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import AppLayout from "@/layouts/AppLayout";
 import DashbordView from "@/views/DashbordView";
 import CreateProjectView from "@/views/projects/CreateProjectView";
 import EditProjectView from "./views/projects/EditProjectView";
+import ProjectDetailsView from "./views/projects/ProjectDetailsView";
 
 const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
         <Route element={<AppLayout />}>
           <Route index element={<DashbordView />} />
           <Route path='/projects/create' element={<CreateProjectView />} />
+          <Route path='/projects/:projectId' element={<ProjectDetailsView />} />
           <Route path='/projects/:projectId/edit' element={<EditProjectView />} />
         </Route>
       </Routes>
---
+++ src/views/projects/ProjectDetailsView.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectDetailsView = () => {
+  return (
+    <div>ProjectDetailsView</div>
+  )
+}
+
+export default ProjectDetailsView
