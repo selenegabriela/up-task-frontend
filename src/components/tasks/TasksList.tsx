7b710372fbc4ba3e23a8bfe4f3a30edb52b9dda8
selene_gabriela <sel_nn@hotmail.com>
2026-03-10 14:46:17 -0600

    changing dolor border matching status

--- src/components/tasks/TasksList.tsx
+++ src/components/tasks/TasksList.tsx
@@ -17,6 +17,14 @@ const initialStatusGroups : GroupedTasks = {
     completed: [],
 }
 
+const statusStyles : {[key: string] : string} = {
+    pending: 'border-t-slate-500',
+    onHold: 'border-t-red-500',
+    inProgress: 'border-t-blue-500',
+    underReview: 'border-t-amber-500',
+    completed: 'border-t-emerald-500',
+}
+
 const statusTranslations : {[key: string] : string} = {
     pending: 'Pending',
     onHold: 'On Hold',
@@ -41,7 +49,7 @@ const TasksList = ({tasks}: TasksListProps) => {
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
-                        <h3 className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}>{statusTranslations[status]}</h3>
+                        <h3 className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}>{statusTranslations[status]}</h3>
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No Tasks</li>
