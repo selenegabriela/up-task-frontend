ca0a4b160fc391427ba93adf6f293cb510e96520
selene_gabriela <sel_nn@hotmail.com>
2026-03-27 16:14:22 -0600

    Validating task exists

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -1,15 +1,33 @@
 import { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getTaskById } from '@/api/TaskApi';
+import { toast } from 'react-toastify';
 
 
 export default function TaskModalDetails() {
+    const params = useParams();
+    const projectId = params.projectId!;
     const navigate = useNavigate();
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
-    const taskId = queryParams.get('viewTask');
+    const taskId = queryParams.get('viewTask')!;
 
     const show = taskId ? true : false
+
+    const {data, isError, error} = useQuery({
+        queryKey: ['task', taskId],
+        queryFn: () => getTaskById({projectId,taskId}),
+        enabled: !!taskId,
+        retry: false
+    });
+    console.log(data);
+    if(isError){
+        toast.error(error.message, {toastId: 'error'});
+        return <Navigate to={`/projects/${projectId}`} />
+
+    }
     return (
         <>
             <Transition appear show={show} as={Fragment}>
